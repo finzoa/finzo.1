4db860f7a9419652a04c326b94599dbefbe6eafe
finzoa <finzobhote678@gmail.com>
2025-01-21 09:37:51 +0545

    password generator and strength checker

--- .c
+++ .c
@@ -1,37 +1,69 @@
-# finzo.1
-
+#password generator and strength checker
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <time.h>
 char getRandomChar()
 {
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$?/-_";
     int index = rand() % (sizeof(charset) - 1);
     return charset[index];
-
 }
+int checkPasswordStrength(const char *password)
+{
+    int length = strlen(password);
+    int hasUpper = 0, hasLower = 0, hasDigit = 0, hasSpecial = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper(password[i]))
+            hasUpper = 1;
+        else if (islower(password[i]))
+            hasLower = 1;
+        else if (isdigit(password[i]))
+            hasDigit = 1;
+        else if (strchr("!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\", password[i]))
+            hasSpecial = 1;//finzo Bhote
+    }
+    int score = hasUpper + hasLower + hasDigit + hasSpecial;
+    if (length >= 12 && score == 4)
+        return 3; // Strong
+    else if (length >= 8 && score >= 3)
+        return 2; // Medium
+    else
+        return 1; //weak
+}
+void evaluatePassword(const char *password)
+{
+    int strength = checkPasswordStrength(password);
+
+    printf("\nPassword: %s\n", password);
+
+    if (strength == 3)
+        printf("Strength: \033[1;32mStrong\033[0m\n");
+    else if (strength == 2)
+        printf("Strength: \033[1;33mMedium\033[0m\n");
+    else
+        printf("Strength: \033[1;31mWeak\033[0m\n");
+}
+
 void generatePassword(int length)
 {
     printf("\033[2J\033[H"); 
     printf("\033[1;36m");    
-    printf("********* Strong Password Generator dev by finzo ********* \n");
+    printf("********* Strong Password Generator and Strength Checker by finzo ********* \n");
     printf("\033[0m");       
-    srand(time(NULL));  
+    char password[256]; 
     while (1)
     {
-        printf("\033[1;35m"); 
-        printf("Generated Password: ");
-        printf("\033[1;32m");  
         for (int i = 0; i < length; i++)
         {
-            printf("%c", getRandomChar());
+            password[i] = getRandomChar();
         }
-        printf("\n");
-        printf("\033[0m");  
-        printf("\n");
-        printf("\033[1;33m");  
-        printf("Do you want to generate another password? (y/n): ");
-        printf("\033[0m");    
+        password[length] = '\0'; 
+        printf("\033[1;35mGenerated Password: \033[1;32m%s\033[0m\n", password);
+        evaluatePassword(password);
+        printf("\n\033[1;33mDo you want to generate another password? (y/n): \033[0m");
         char choice;
         scanf(" %c", &choice);
         if (choice == 'n' || choice == 'N')
@@ -41,9 +73,33 @@ void generatePassword(int length)
         printf("\n");
     }
 }
+
 int main()
 {
-    int passwordLength = 10;  
+    srand(time(NULL)); 
+    int passwordLength;
+    printf("\033[1;34m");
+    printf("Enter the desired password length: ");
+    printf("\033[0m");
+    scanf("%d", &passwordLength);
+    if (passwordLength < 1)
+    {
+        printf("\033[1;31m");
+        printf("Password length must be greater than 0!\n");
+        printf("\033[0m");
+        return 1;
+    }
     generatePassword(passwordLength);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
